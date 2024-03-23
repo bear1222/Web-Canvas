55bdfc50565e9aa2479d646a8c32e32a92545006
bear1222 <doo1222.tw@gmail.com>
2024-03-23 22:00:20 +0800

    draw rectangle and circle finish

--- app.js
+++ app.js
@@ -3,11 +3,10 @@ let canvas = document.getElementById('paper');
 let ctx = canvas.getContext('2d');
 let thicknessSelector = document.getElementById('thickness');
 let colorSelector = document.getElementById('color');
-let selectedMode = "brush";
+let selectedMode = "rect";
 
-
-canvas.height = window.innerHeight;
-canvas.width = window.innerWidth;
+let savedCanvas = document.getElementById('savedpaper');
+let savedCtx = savedCanvas.getContext('2d');
 
 cursor = {x: 0, y: 0};
 start = {x: 0, y: 0};
@@ -51,6 +50,7 @@ erase = {
 
 createRect = {
     start: function(){
+        savedCtx.drawImage(canvas, 0, 0);
         ctx.globalCompositeOperation = "source-over";
         ctx.beginPath();
         ctx.moveTo(cursor.x, cursor.y);
@@ -58,46 +58,50 @@ createRect = {
         start.y = cursor.y;
         console.log("start:", start);
         canvas.addEventListener('mousemove', this.move);
-
     }, 
     move: function(){
-        // add preview
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.beginPath();
+        ctx.rect(start.x, start.y, cursor.x - start.x, cursor.y - start.y);
+        ctx.stroke();
     }, 
     end: function(){
         canvas.removeEventListener('mousemove', this.move);
-        ctx.rect(start.x, start.y, cursor.x - start.x , cursor.y - start.y);
-        console.log(start, cursor);
-        ctx.stroke();
+        savedCtx.drawImage(canvas, 0, 0, canvas.width, canvas.height);
+        ctx.drawImage(savedCanvas, 0, 0, canvas.width, canvas.height);
+        savedCtx.clearRect(0, 0, canvas.width, canvas.height);
     }
 
 };
 
 createCircle = {
     start: function(){
+        savedCtx.drawImage(canvas, 0, 0);
         ctx.globalCompositeOperation = "source-over";
-        ctx.moveTo(cursor.x, cursor.y);
         ctx.beginPath();
+        ctx.moveTo(cursor.x, cursor.y);
         start.x = cursor.x;
         start.y = cursor.y;
         console.log("start:", start);
         canvas.addEventListener('mousemove', this.move);
-
     }, 
     move: function(){
         // add preview
-    }, 
-    end: function(){
-        canvas.removeEventListener('mousemove', this.move);
         const nowx = cursor.x;
         const nowy = cursor.y;
         const x = (nowx + start.x) / 2;
         const y = (nowy + start.y) / 2;
-        // const r = min(nowx - x, nowy - y);
-        const r = Math.abs(nowx - x);
-        console.log(x, y, r);
+        const r = Math.min(Math.abs(nowx - x), Math.abs(nowy - y));
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.beginPath();
         ctx.arc(x, y, r, 0, 2 * Math.PI);
-        console.log(start, cursor);
         ctx.stroke();
+    }, 
+    end: function(){
+        canvas.removeEventListener('mousemove', this.move);
+        savedCtx.drawImage(canvas, 0, 0, canvas.width, canvas.height);
+        ctx.drawImage(savedCanvas, 0, 0, canvas.width, canvas.height);
+        savedCtx.clearRect(0, 0, canvas.width, canvas.height);
     }
 };
 
